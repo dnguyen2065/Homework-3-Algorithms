3bee5785a59c0dee638717e887cfda91d3849b02
dnguyen2065 <dnguyen2065@gmail.com>
2022-11-22 16:05:04 -0500

    inversion counter done

--- src/App.java
+++ src/App.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Scanner myObj = new Scanner(System.in);
-        System.out.print("Enter text to check palindrome: ");
-        String string = myObj.nextLine();
-        Palindrome p = new Palindrome();
-        boolean finalBool = p.palindromeCheck(string, 0);
-        System.out.println(finalBool);
+        // Scanner myObj = new Scanner(System.in);
+        // System.out.print("Enter text to check palindrome: ");
+        // String string = myObj.nextLine();
+        // Palindrome p = new Palindrome();
+        // boolean finalBool = p.palindromeCheck(string, 0);
+        // System.out.println(finalBool);
+
+        InversionCount ic = new InversionCount();
+        int[] intArr = { 4, 1, 2, 3, 6, 8, 1, 2, 123, 5, 7, 56, 34, 3245, 8, 978, 0, 954, 34, 345, };
+        System.out.println(ic.easyInversionCount(intArr));
+        System.out.println(ic.fastInversionCount(intArr));
     }
 }
--- src/InversionCount.java
+++ src/InversionCount.java
@@ -1,11 +1,77 @@
 public class InversionCount {
+    int counter = 0;
 
-    public void easyInversionCount(int[] inputArr) {
+    public int easyInversionCount(int[] inputArr) {
+        int counter = 0;
+        for (int i = 0; i < inputArr.length; i++) {
+            for (int j = i + 1; j < inputArr.length; j++) {
+                if (inputArr[i] > inputArr[j]) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    private void merge(int arr[], int first, int middle, int last) {
+        int i;
+        int k;
+        int j;
+
+        int firstHalf = middle - first + 1;
+        int lasthalf = last - middle;
+
+        int[] leftArray = new int[firstHalf];
+        int[] rightArray = new int[lasthalf];
+
+        for (i = 0; i < firstHalf; i++) {
+            leftArray[i] = arr[first + i];
+        }
+        for (j = 0; j < lasthalf; j++) {
+            rightArray[j] = arr[middle + j + 1];
+        }
+        i = 0;
+        j = 0;
+        k = first;
+
+        while (i < firstHalf && j < lasthalf) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+                counter += firstHalf - i;
+            }
+            k++;
+        }
+        while (i < firstHalf) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while (j < lasthalf) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
 
     }
 
-    public void fastInversionCount() {
+    private void mergeSort(int[] arr, int first, int last) {
+        if (first < last) {
+            int middle = (first + last) / 2;
+            mergeSort(arr, first, middle);
+            mergeSort(arr, middle + 1, last);
+            merge(arr, first, middle, last);
 
+        }
+
+    }
+
+    public int fastInversionCount(int[] inputArr) {
+        mergeSort(inputArr, 0, inputArr.length - 1);
+        return counter;
     }
 
 }
