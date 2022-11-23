047a54240c580d1579eb32a73be13fed49c3cb80
dnguyen2065 <dnguyen2065@gmail.com>
2022-11-22 21:52:20 -0500

    part 3 done

--- src/App.java
+++ src/App.java
@@ -4,22 +4,28 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws Exception {
+        // AFTER ENTERING THE PALINDROME QUESTIONS 2 AND 3 WILL PRINT IMMEDIATELY AFTER
+        // WITH LABELS
         // Q1---------------------------------------
-        // Scanner myObj = new Scanner(System.in);
-        // System.out.print("Enter text to check palindrome: ");
-        // String string = myObj.nextLine();
-        // Palindrome p = new Palindrome();
-        // boolean finalBool = p.palindromeCheck(string, 0);
-        // System.out.println(finalBool);
+        System.out.println("Q1---------------------------------------");
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter text to check palindrome: ");
+        String string = myObj.nextLine();
+        Palindrome p = new Palindrome();
+        boolean finalBool = p.palindromeCheck(string, 0);
+        System.out.println(finalBool);
         // Q2--------------------------------------
-        // InversionCount ic = new InversionCount();
-        // int[] intArr = { 4, 1, 2, 3, 6, 8, 1, 2, 123, 5, 7, 56, 34, 3245, 8, 978, 0,
-        // 954, 34, 345, };
-        // System.out.println(ic.easyInversionCount(intArr));
-        // System.out.println(ic.fastInversionCount(intArr));
+        System.out.println("Q2---------------------------------------");
+        InversionCount ic = new InversionCount();
+        int[] intArr = { 4, 1, 2, 3, 6, 8, 1, 2, 123, 5, 7, 56, 34, 3245, 8, 978, 0,
+                954, 34, 345, };
+        System.out.println(ic.easyInversionCount(intArr));
+        System.out.println(ic.fastInversionCount(intArr));
         // Q3---------------------------------------
+        System.out.println("Q3---------------------------------------");
         BinaryReflectedGrayCode brcg = new BinaryReflectedGrayCode();
         ArrayList<String> input = brcg.BRCG(4);
-        System.out.println(brcg.nameSequence(input));
+        // System.out.println(brcg.nameSequence(input));
+        brcg.allTogether(brcg.nameSequence(input), input);
     }
 }
--- src/BinaryReflectedGrayCode.java
+++ src/BinaryReflectedGrayCode.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.swing.SortingFocusTraversalPolicy;
+
 public class BinaryReflectedGrayCode {
     public ArrayList<String> BRCG(int n) {
         ArrayList<String> L = new ArrayList<>();
@@ -36,21 +38,95 @@ public class BinaryReflectedGrayCode {
         int Chris = 1;
         int Dylan = 0;
         String[] split;
-        for (int i = 0; i < arr.size(); i++) {
+        String[] split2;
+        for (int i = 0; i < arr.size() - 1; i++) {
             split = arr.get(i).split("");
-            if (split[Alice].equals("1")) {
+            split2 = arr.get(i + 1).split("");
+            if (!split[Alice].equals(split2[Alice])) {
                 orderedList.add("Alice");
-            } else if (split[Bob].equals("1")) {
+            } else if (!split[Bob].equals(split2[Bob])) {
                 orderedList.add("Bob");
-            } else if (split[Chris].equals("1")) {
+            } else if (!split[Chris].equals(split2[Chris])) {
                 orderedList.add("Chris");
-            } else if (split[Dylan].equals("1")) {
+            } else if (!split[Dylan].equals(split2[Dylan])) {
                 orderedList.add("Dylan");
             }
 
         }
         return orderedList;
 
+    }
+
+    public void allTogether(ArrayList<String> names, ArrayList<String> GC) {
+        System.out.println("Index, Gray Code, Child(ren) in Photo, Action");
+        int counterA = 0;
+        int counterB = 0;
+        int counterC = 0;
+        int counterD = 0;
+        ArrayList<String> photoList = new ArrayList<>();
+        ArrayList<String> actionList = new ArrayList<>();
+        for (int i = 0; i < names.size(); i++) {
+            System.out.print(i + " | ");
+            System.out.print(GC.get(i + 1) + " | ");
+            if (names.get(i).equals("Alice")) {
+                counterA++;
+                if (counterA % 2 != 0) {
+                    photoList.add(names.get(i));
+                    actionList.add(names.get(i) + " In");
+                } else {
+                    photoList.remove(names.get(i));
+                    actionList.add(names.get(i) + " Out");
+                }
+            }
+            if (names.get(i).equals("Bob")) {
+                counterB++;
+                if (counterB % 2 != 0) {
+                    photoList.add(names.get(i));
+                    actionList.add(names.get(i) + " In");
+                } else {
+                    photoList.remove(names.get(i));
+                    actionList.add(names.get(i) + " Out");
+                }
+            }
+            if (names.get(i).equals("Chris")) {
+                counterC++;
+                if (counterC % 2 != 0) {
+                    photoList.add(names.get(i));
+                    actionList.add(names.get(i) + " In");
+                } else {
+                    photoList.remove(names.get(i));
+                    actionList.add(names.get(i) + " Out");
+                }
+            }
+            if (names.get(i).equals("Dylan")) {
+                counterD++;
+                if (counterD % 2 != 0) {
+                    photoList.add(names.get(i));
+                    actionList.add(names.get(i) + " In");
+                } else {
+                    photoList.remove(names.get(i));
+                    actionList.add(names.get(i) + " Out");
+                }
+            }
+
+            for (int j = 0; j < photoList.size(); j++) {
+                System.out.print(photoList.get(j));
+                if (j != photoList.size() - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.print(" | ");
+            for (int k = 0; k < actionList.size(); k++) {
+                System.out.print(actionList.get(k));
+                if (k != actionList.size() - 1) {
+                    System.out.print(", ");
+                }
+
+            }
+            System.out.println("");
+            actionList.clear();
+
+        }
     }
 
 }
